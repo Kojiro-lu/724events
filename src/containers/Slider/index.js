33a694678e3caf81cb1cd0c6eebfbf721c6b688e
Anthony <kojiro5656i@gmail.com>
2025-04-07 10:05:38 +0200

    Modification du Slide pour l'affichage des images dans le bon ordre

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -7,9 +7,13 @@ import "./style.scss";
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
-  const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
-  );
+
+  // On clone le tableau avec slice() pour éviter de modifier data.focus directement
+  // Puis on trie du plus récent au plus ancien avec la version conditionnelle >
+  const byDateDesc = data?.focus
+    .slice()
+    .sort((evtA, evtB) => (new Date(evtA.date) > new Date(evtB.date) ? -1 : 1));
+
   const nextCard = () => {
     setTimeout(() => setIndex(index < byDateDesc.length ? index + 1 : 0), 5000);
   };
